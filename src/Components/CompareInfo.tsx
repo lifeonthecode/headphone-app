426e25a7c1e9c3b017ac593d0e76c39b625f0f7e
lifeonthecode <mohammodjuwelislam@gmail.com>
2026-02-04 02:20:48 +0600

    alignment adjust

--- src/Components/CompareInfo.tsx
+++ src/Components/CompareInfo.tsx
@@ -21,7 +21,7 @@ const CompareInfo: React.FC<Props> = ({item}) => {
     return (
         <div className="bg-white pt-6 md:pl-6 md:pr-6 max-h-350 h-full pb-16 md:rounded-3xl flex flex-col gap-10">
 
-            <div className="bg-[#f5f5f5] py-6 md:py-0 md:p-6 md:rounded-3xl flex flex-col items-center justify-between">
+            <div className="bg-[#f5f5f5] py-6 md:pl-6 md:pr-6 md:rounded-3xl flex flex-col items-center justify-between">
                 <h3 className="text-2xl md:text-4xl text-[#52483e] font-semibold capitalize">{item?.title}</h3>
                 <img src={item?.image} className="min-w-37.5 md:max-w-75 w-full min-h-37.5 md:min-h-75 h-full object-contain" alt={item?.title} />
                 <span className="text-lg text-[#8b8782] font-normal mb-4">Start at ${item?.price}</span>
